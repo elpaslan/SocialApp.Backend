b7966deb6aadc9375feb48f97bd16277ba40bb51
Alpaslan Dulkadir <alpaslandulkadir@gmail.com>
2021-06-17 01:44:18 +0300

    initial commit

---
+++ SocialApp.Backend.Webapi/Data/SocialContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.Backend.Webapi.Data
+{
+    public class SocialContext : DbContext
+    {
+        public SocialContext(DbContextOptions<SocialContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
